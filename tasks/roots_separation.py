bf9a3691b047a4fda94b2cb831d7e1b782cb3507
Alex Belov <markelofaleksei@gmail.com>
2023-04-24 23:20:10 +0300

    Фикс

--- tasks/roots_separation.py
+++ tasks/roots_separation.py
@@ -1,31 +1,68 @@
-from misc import find_interval, bisection_method
+from sympy import symbols, diff, solve
 
-def roots_separation_5():
-    degree = int(input("Введите наибольшую степень уравнения: "))
-    coeffs = []
-    for i in range(degree, -1, -1):
-        coeff = float(input(f"Введите коэффициент перед x^{i}: "))
-        coeffs.append(coeff)
-    powers = list(range(degree, -1, -1))
-
-    # Выполняем пункт 4 программы (уточнение корней)
-    # ... (как и раньше)
-
-    # Определяем функцию f, left и right
-    f = lambda x: sum([coeff * x ** power for coeff, power in zip(coeffs, powers)])
-    left, right = find_interval(coeffs, powers)
-
-    # Запрос на ввод точности
-    epsilon = float(input("Введите точность (например, 0.001): "))
-
-    print("Выберите метод решения уравнения:")
-    print("1. Метод итераций")
-    print("2. Метод половинного деления")
-    method_choice = input()
-
-    if method_choice == '1':
-        # Реализуйте метод итераций здесь
-        pass
-    elif method_choice == '2':
-        root, tolerance = bisection_method(f, left, right, epsilon)
-        print(f"x = {root} +- {tolerance}")
+def separate_roots_5():
+    y = 0
+    x = symbols('x')
+    k = 0
+    degree = int(input('Введите степень уравнения: '))
+    print('Введите коэфицент от меньшего к большему:')
+    for i in range(0, degree + 1):
+        k = int(input())
+        y += k * x ** i
+    print(f"Ваше уравнение: {str(y)} = 0")
+    print("Отделим корни уравнения")
+    print("Построим таблицу значений")
+    k = 0
+    n = 0
+    z = 0
+    za = 0
+    zm = 0
+    xlist = list()
+    while (k < degree):
+        za = z
+        z = y.subs(x, n)
+        print(f'y = {str(z)}, при x = {str(n)}')
+        if za * z < 0:
+            k += 1
+            xlist.append(str(n))
+            a = [n - 1, n]
+            print(f'Корень уравнения: {str(a)}')
+        za = zm
+        zm = y.subs(x, -n)
+        print(f'y = {str(zm)}, при x = {str(-n)}')
+        if za * zm < 0:
+            k += 1
+            xlist.append(str(-n + 1))
+            a = [-n, -n + 1]
+            print(f'корень {str(a)}')
+        n += 1
+    print('Функция непрерывна')
+    print('Проверим отрезки на монотонность')
+    print(f'Производная равна: {str(diff(y, x))}')
+    xnull = solve(diff(y, x))
+    print(f'Экстремумы 1 порядка равны {str(xnull)}')
+    n = 0
+    while (n < degree):
+        i = 0
+        xdel = False
+        z = int(xlist[n])
+        a = [z - 1, z]
+        print(f'Проверим отрезок {str(a)}')
+        while (i < degree - 1):
+            zm = xnull[i]
+            if zm > z or zm < z - 1:
+                print(f'Функция монотонна на отрезке {str(zm)}')
+            else:
+                print('Функция не монотонная')
+                xdel = True
+                break
+            i += 1
+        if not xdel:
+            n += 1
+    n = 0
+    print('Корни отделены')
+    while (n < degree):
+        z = int(xlist[n])
+        a = [z - 1, z]
+        print(str(a))
+        n += 1
