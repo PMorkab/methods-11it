18de74c06bfe85583e57ba8ac07c64a5e22ea4cc
Alex Belov <markelofaleksei@gmail.com>
2023-04-30 16:03:41 +0300

    Delete tasks directory

--- tasks/absolute_error.py
+++
@@ -1,5 +0,0 @@
-from misc import absolute_error
-
-def absolute_error_1():
-    x1, x2 = map(float, input("Введите два числа через пробел: ").split())
-    print(absolute_error(x1, x2))
--- tasks/relative_error.py
+++
@@ -1,5 +0,0 @@
-from misc import relative_error
-
-def relative_error_2():
-    x1, x2 = map(float, input("Введите два числа через пробел: ").split())
-    print(relative_error(x1, x2))
